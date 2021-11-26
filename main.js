de1cdf3cbeb0f77ecf069da07870d684b3d5d8cd
varunmahee <84283092+varunmahee@users.noreply.github.com>
2021-11-26 16:12:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var mouseEvent= "empty";
+var last_position_of_x, last_position_of_y;
+    
+    
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");    
+    radius = 1;
+    color = "black";
+    width_of_line = 1;
+    canvas.addEventListener("mousedown", my_mousedown);
+    function my_mousedown(e)
+    {
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width_of_line").value;
+        mouseEvent = "mouseDown";
+    }
+
+    canvas.addEventListener("mousemove", my_mousemove);
+    function my_mousemove(e)
+    {
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    
+        if (mouseEvent == "mouseDown") {
+            console.log("Current position of x and y coordinates = ");
+            console.log("x =" + current_position_of_mouse_x + "y =" + current_position_of_mouse_y);
+            ctx.beginPath();
+            ctx.strokeStyle = color;
+            ctx.lineWidth = width_of_line;
+            ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius,0 , 2 * Math.PI);
+            ctx.stroke();
+    }
+
+    if (mouseEvent == "mouseleave") {
+        console.log("Current position of x and y coordinates = ");
+        console.log("y =" + current_position_of_mouse_y + "x =" + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_y, current_position_of_mouse_x, radius,0 , 2 * Math.PI);
+        ctx.stroke();
+}
+    last_position_of_x = current_position_of_mouse_x;
+    last_position_of_y = current_position_of_mouse_y;
+
+    canvas.addEventListener("mouseup", my_mouseup);
+    function my_mouseup(e)
+    {
+        mouseEvent = "mouseUp";
+    }
+
+    canvas.addEventListener("mouseleave", my_mouseleave);
+    function my_mouseleave(e)
+    {
+        mouseEvent = "mouseleave";
+    }
+
+}
+function clearArea(){   
+   
+       ctx.clearRect(0, 0,  canvas.width, canvas.height);
+    
+}
